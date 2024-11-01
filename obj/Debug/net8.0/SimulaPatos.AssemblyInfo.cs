4af18e177388796c6f1ed2f224c10e5ccc82e5f8
Fábio Cordeiro <fabiio@pucminas.br>
2024-11-01 19:57:34 -0300

    criando classe IronDuck

--- obj/Debug/net8.0/SimulaPatos.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimulaPatos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimulaPatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccb7175891e48c7eb656b42e2e5da7640248934c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimulaPatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimulaPatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
