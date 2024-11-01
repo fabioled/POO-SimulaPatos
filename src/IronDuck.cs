5ee24ada56ae82dc3899fc8c842f12a45b5020fc
Gabriel Médici <142552114+medicegabriel@users.noreply.github.com>
2024-11-01 20:16:39 -0300

    Update IronDuck.cs

--- src/IronDuck.cs
+++ src/IronDuck.cs
@@ -1,1 +1,12 @@
+class IronDuck : Duck
+{
+    public override void Display()
+    {
+        Console.WriteLine("Displaying a Iron Duck");
+    }
 
+    public void Throw(){
+        Console.WriteLine("Throwing the Iron Duck, this gonna hurt!");
+    }
+
+}
